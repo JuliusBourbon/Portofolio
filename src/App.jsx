6715a605eedd5a34862a0e8b32f0051e39032e3d
JuliusBourbon <naherrrrr@gmail.com>
2025-10-24 12:28:42 +0700

    Update view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import HomePage from "./components/HomePage";
+
 export default function App() {
   return(
-    <div className="text-red-600 flex justify-center items-center w-full">
-      <h1>HIIIII!!!!!</h1>
+    <div className="w-full min-h-screen flex flex-col overflow-x-hidden">
+      <HomePage/>
     </div>
   )
 }
