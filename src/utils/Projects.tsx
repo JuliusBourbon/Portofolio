8cf37824e8f9f1a9cba3149297614f89e407949a
JuliusBourbon <naherrrrr@gmail.com>
2026-04-25 16:59:06 +0700

    feat: add 3 project to Projects Section

--- src/utils/Projects.tsx
+++ src/utils/Projects.tsx
@@ -17,6 +17,20 @@ const scribely_1 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v177708912
 const scribely_2 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777089130/scribely_2_eldcvd.png'
 const scribely_3 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777089130/scribely_3_kgqswi.png'
 
+// Visualization of Indonesian employment data Image
+const visualization_1 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777102779/dataVisualization_2_ccqodr.png'
+const visualization_2 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777102779/dataVisulization_1_s7bldx.png'
+
+// Sentiment Analysis Image
+const sentiment_1 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777108481/sentiment_1_pivdti.png'
+const sentiment_2 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777108481/sentiment_2_scou9a.png'
+const sentiment_3 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777108482/sentiment_3_mti7zl.png'
+
+// Terna Image
+const terna_1 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777110820/Terna_1_c2wanj.png'
+const terna_2 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777110821/Terna_2_q7vqyf.png'
+const terna_3 = 'https://res.cloudinary.com/dnn3mm02t/image/upload/v1777110822/Terna_3_eq3bvh.png'
+
 export interface ProjectsItem {
   id: number;
   title: string;
@@ -129,6 +143,84 @@ export const projects: ProjectsItem[] = [
         { name: 'Node.js', category: 'Backend' },
         { name: 'Express', category: 'Backend' },
 
+      ],
+    },
+  },
+  {
+    id: 4,
+    title: 'Visualization of Indonesian employment data',
+    description: 'A Dashboard to visualize Indonesian employment data',
+    image: visualization_1,
+    detail: {
+      overview:
+        'A dashboard to visualize Indonesian employment data across different regions and time periods.',
+      role: 'Data Analyst',
+      year: '2025',
+      duration: '3 weeks',
+      liveUrl: 'https://visualisasi-data-ketenagakerjaan.streamlit.app/',
+      githubUrl: 'https://github.com/JuliusBourbon/visualisasi-data-ketenagakerjaan',
+      highlights: [
+        'Data collection and preprocessing using Python and Pandas to clean and structure the employment data for analysis.',
+        'Data visualization using Streamlit and Plotly to create interactive charts and graphs that highlight employment trends and patterns across different regions and time periods.',
+        'Implemented features such as filtering by region, time period, and employment type to allow users to explore the data in depth.'
+      ],
+      gallery: [visualization_2, visualization_1],
+      stack: [
+        { name: 'Python', category: '' },
+        { name: 'Pandas', category: '' },
+        { name: 'Streamlit', category: '' },
+        { name: 'Plotly', category: '' },
+      ],
+    },
+  },
+  {
+    id: 5,
+    title: 'Sentiment Analysis on X Tweets',
+    description: 'Conducting sentiment analysis on tweets about \'kemenkeu\' on platform X',
+    image: sentiment_1,
+    detail: {
+      overview:
+        'In September 2025, there was a change in the Indonesian Ministry of Finance. This sentiment analysis was conducted to assess people\'s reactions on platform X.',
+      role: 'Data Analyst',
+      year: '2025',
+      duration: '1 weeks',
+      liveUrl: '#',
+      githubUrl: 'https://github.com/JuliusBourbon/Analisis-Sentimen-Kemenkeu',
+      highlights: [
+        'Data collection using X API to gather tweets mentioning "kemenkeu" over a specified time period.',
+        'Data preprocessing using Python and NLTK to clean the tweet text, remove stop words, and prepare the data for sentiment analysis.',
+        'Visualized wordcloud of the most frequently used words in the tweets to identify common themes and topics of discussion.',
+        'Conducted sentiment analysis using a pre-trained model to classify the tweets as positive, negative, or neutral'
+      ],
+      gallery: [sentiment_1, sentiment_2, sentiment_3],
+      stack: [
+        { name: 'Python', category: 'Backend' },
+        { name: 'Ipynb', category: 'Backend' },
+        { name: 'NLTK', category: 'Backend' },
+      ],
+    },
+  },
+  {
+    id: 6,
+    title: 'Terna',
+    description: 'A Chinese poker sorter',
+    image: terna_1,
+    detail: {
+      overview:
+        'Terna is a Chinese poker sorter that helps players organize their cards according to the rules of Chinese poker. It provides an intuitive interface for players to input their cards and receive optimal sorting suggestions based on the game\'s hand ranking system.',
+      role: 'Fullstack Developer',
+      year: '2025',
+      duration: '1 months',
+      liveUrl: 'https://playterna.vercel.app/',
+      githubUrl: 'https://github.com/JuliusBourbon/Terna',
+      highlights: [
+        'Implemented a user-friendly interface using React and Tailwind CSS for players to input their cards and receive sorting suggestions.',
+        'Using Greedy algorithm to generate optimal card arrangements based on the hand ranking system of Chinese poker.',
+      ],
+      gallery: [terna_1, terna_2, terna_3],
+      stack: [
+        { name: 'React', category: 'Frontend' },
+        { name: 'Tailwind', category: 'Frontend' },
       ],
     },
   },
